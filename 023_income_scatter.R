1cb5dc9e6fc084fae637cc10b36cecf5682ef70f
PhattSeal <66963526+ChrisAndrade2020@users.noreply.github.com>
2025-05-30 07:23:15 -0400

    023 start

---
+++ 023_income_scatter.R
@@ -0,0 +1,7 @@
+# Purpose: Aggregate rat calls and ACS data to tract level
+#          and compute call-rate vs. median income for Tableau.
+
+# 0) Load required packages
+library(tidyverse)
+library(sf)
+
